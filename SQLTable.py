e1931a9133d58bbaf4281b7c820b1632817e2e52
Nikita Gusev <gusevnikita09@gmail.com>
2025-10-21 15:32:18 +0300

    импорт/экспорт данных

--- SQLTable.py
+++ SQLTable.py
@@ -1,7 +1,7 @@
 import sqlalchemy
 import pandas as pd
 import pymysql
-from sqlalchemy import create_engine, Column, Integer, String, Date, Boolean
+from sqlalchemy import create_engine, Column, Integer, String, Date, Boolean, text
 from sqlalchemy.dialects.mysql import insert
 from sqlalchemy.orm import declarative_base, sessionmaker
 from datetime import date
@@ -25,9 +25,15 @@ def create_table():
     Base.metadata.create_all(engine)
     print("Таблица создана.")
 
-def drop_table():
-    Base.metadata.drop_all(engine)
-    print("Таблица удалена.")
+def drop_table(table_name):
+    with engine.connect() as conn:
+        conn.execute(text(f"DROP TABLE IF EXISTS `{table_name}`;"))
+        print(f"Таблица '{table_name}' удалена (если существовала).")
+
+def show_table(table_name, limit=10):
+    df = pd.read_sql_query(f"SELECT * FROM `{table_name}` LIMIT {limit}", con=engine)
+    print(df)
+
 
 def insert_users():
     session = Session()
@@ -71,19 +77,42 @@ def update_user_all(user_id, name, email, birthdate, is_active, phone, city):
         print(f"Пользователь с id={user_id} не найден.")
         session.close()
 
-drop_table()
-create_table()
-insert_users()
-update_user_all(
-    user_id=1,
-    name="Иван Петров",
-    email="ivanpetrov@example.com",
-    birthdate=date(1991, 2, 2),
-    is_active=False,
-    phone="999999999",
-    city="Екатеринбург"
-)
-select_all()
+def export_to_csv(filename):
+    session = Session()
+    df = pd.read_sql(session.query(User).statement, session.bind)
+    df.to_csv(filename, index=False)
+    print(f"Данные экспортированы в {filename}")
+    session.close()
+
+def import_csv(csv_path, table_name=None):
+    df = pd.read_csv(csv_path)
+    print("Столбцы файла:", df.columns.tolist())
+    if table_name is None:
+        import os
+        table_name = os.path.splitext(os.path.basename(csv_path))[0]
+    df.to_sql(table_name, con=engine, if_exists='replace', index=False)
+    print(f"CSV-таблица '{csv_path}' импортирована в таблицу MySQL '{table_name}'")
+
+
+# create_table()
+# insert_users()
+# update_user_all(
+#     user_id=1,
+#     name="Иван Петров",
+#     email="ivanpetrov@example.com",
+#     birthdate=date(1991, 2, 2),
+#     is_active=False,
+#     phone="999999999",
+#     city="Екатеринбург"
+# )
+# select_all()
+# export_to_csv("users1.csv")
+# df = pd.read_csv("users1.csv")
+# print(df)
+# import_csv('diabetes.csv', table_name='diabetes')
+# export_to_csv('diabetes.csv')
+# drop_table('diabetes')
+# show_table('diabetes')
 
 
 connection = engine.connect()
